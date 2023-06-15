0d0b4f0cf6395f35f8ab55b6006c381d486ed87d
Nikku kumar <78784247+nikkuveejar@users.noreply.github.com>
2023-06-16 05:00:07 +0530

    commit

--- 12 Arrays/reverse.java
+++ 12 Arrays/reverse.java
@@ -1,4 +1,6 @@
 import java.util.*;
+
+// Reversing an Array
 public class reverse {
     public static void reversearray(int numbers[]){
     int first=0, last= numbers.length-1;
@@ -10,9 +12,26 @@ public class reverse {
         last--;
         }
     }    
+    
+    // Pairs in an array
+    public static void pairs(int numbers[]){
+        int pairs =0;
+        for (int i=0; i<numbers.length; i++){
+            int current = numbers[i];
+            for(int j= i+1; j<numbers.length; j++){
+                System.out.print( "(" +current + "," +numbers[j]+ ")");
+                pairs++;
+                
+            }
+            System.out.println();
+        }
+        System.out.println("Total number of pairs =" +pairs);
+    }
 public static void main(String [] args){
     int numbers[]={2, 4, 6, 8, 10};
+    pairs(numbers);
     reversearray(numbers);
+    System.out.print("Reversed arrays is ");
     for(int i=0; i<numbers.length; i++){
         System.out.print(numbers[i]+ " ");
     }
